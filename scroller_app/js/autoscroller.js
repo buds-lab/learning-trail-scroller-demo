5f568c0d9eba957cad7849f4d61ba99901991782
Prageeth <p.jayathissa@gmail.com>
2019-01-14 17:02:53 +0800

    automation working

--- scroller_app/js/autoscroller.js
+++ scroller_app/js/autoscroller.js
@@ -4,15 +4,17 @@
 
 var autoscroller = function(){
 
-	console.log("Started autoscrolling")
-	console.log(window.location.pathname)
-	const pathnames = ["/index.html", "/water_trail.html", "/wellness_trail.html", "/tropical_architecture_trail.html", "/hybrid_cooling_trail.html", "/net_zero_energy.html", "/biophilic_trail.html"]
+
+	const pathnames = ["/index.html", "/water_trail.html", "/wellness_trail.html", "/tropical_architecture_trail.html", "/hybrid_cooling_trail.html", "/net_zero_energy_trail.html", "/biophilic_trail.html"]
 	
 	var currentPathnameIndex = pathnames.indexOf(window.location.pathname)
+	console.log("body offset height " + document.body.offsetHeight)
+	console.log("window innder height " + window.innerHeight)
 
 	var autoscroll = setInterval(function(){ 
-		window.scrollBy(0,2); 
-		if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+		window.scrollBy(0,1); 
+
+		if ((window.innerHeight + window.scrollY +10) >= document.body.offsetHeight) {
 		console.log("At bottom of page");
 		window.scrollTo(0,0);
 
@@ -26,7 +28,7 @@ var autoscroller = function(){
 	}
 		
 
-			}, 10);
+			}, 50);
 	console.log("Autoscroller run")
 
 
